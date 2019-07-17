3f338c71be0205505288a78b728467e0cd57c088
charliew <charliew@yeetalk.net>
2019-07-17 17:54:05 +0800

    fix

--- main.go
+++ main.go
@@ -10,9 +10,18 @@ import (
 func main()  {
 	router := gin.Default()
 	_ = router.GET("/notify", func(c *gin.Context) {
+		log.Println("GET")
 		rawData, e := c.GetRawData()
 		if e == nil {
 			log.Fatal(e)
+		}
+		log.Println(fmt.Sprintf("notify---body: %v", rawData, ))
+	})
+	_ = router.POST("/notify", func(c *gin.Context) {
+		log.Println("POST")
+		rawData, e := c.GetRawData()
+		if e == nil {
+			log.Println("err")
 		}
 		log.Println(fmt.Sprintf("notify---body: %v", rawData, ))
 	})
